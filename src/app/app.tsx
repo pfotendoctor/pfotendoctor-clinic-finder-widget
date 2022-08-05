d937eb9a90f4b00c432f90a28c655091812f146f
Daniel Mikolai <>
2022-08-05 15:46:45 +0200

    delete

--- src/app/app.tsx
+++
@@ -1,8 +0,0 @@
-import { h, JSX, render } from "preact";
-import { DefaultHome } from "./components/home/Home";
-
-window.ClinFinder = function (idElement, param1, param2) {
-	let config = { idElement, param1, param2 };
-	render(<DefaultHome idElement={idElement} param1={param1} param2={param2} />, document.body);
-	return this;
-};
